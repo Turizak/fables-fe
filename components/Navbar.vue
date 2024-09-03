2a0594a9d0a21f85b8226aba5e195f28d355720a
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-09-03 19:09:44 -0400

    added navbar and footer

---
+++ components/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav>
+    <nav className="bg-black text-white">
+      <ul className="flex justify-between">
+        <li className="p-2 h-12"></li>
+        <div className="flex">
+          <li className="p-2"></li>
+          <li className="p-2"></li>
+        </div>
+      </ul>
+    </nav>
+  </nav>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen">
+    <Navbar />
     <slot />
+    <Footer />
   </div>
 </template>
