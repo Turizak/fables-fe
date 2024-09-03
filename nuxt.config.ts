3800563622099c56bea6bf70cab4c967613e594b
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-09-03 18:19:57 -0400

    added tailwind and home page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Fables"
+    },
+  },
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss'],
 })
