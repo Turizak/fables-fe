7862a95c0c8cc82a9213513b3fddd50d4986d44f
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-09-08 11:58:33 -0400

    updated tailwind config and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,16 +8,21 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap',
         },
+        {
+          rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg',
+        }
       ], 
+      
     },
   },
+  compatibilityDate: '2024-04-03',
+  css: ['~/assets/css/tailwind.css'],
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
   runtimeConfig: {
     // Public keys that are exposed to the client
     public: {
       baseURL: process.env.BASE_URL
     }
   },
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@pinia/nuxt'],
 });
