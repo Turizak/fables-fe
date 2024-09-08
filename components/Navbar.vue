07224f6ae556596613290c15299b645338d17148
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-09-08 18:59:28 -0400

    added logout and icon

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,33 +1,62 @@
 <script setup>
+import { useAuthStore } from '~/stores/authStore';
+
+const authStore = useAuthStore();
 const isOpen = ref(false);
+
+async function logout() {
+  authStore.clearTokens();
+  await navigateTo('/login');
+}
 </script>
 
 <template>
   <nav>
-    <ul class="flex bg-green-400 text-black">
-      <li class="p-2">
-        <UButton @click="isOpen = true">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="size-5"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </UButton>
-        <USlideover v-model="isOpen" :overlay="false" side="left" width="5xl">
-          <div class="flex flex-col">
-            <ul>
-              <li class="h-6"></li>
-              <li>
-                <NuxtLink to="/">
+    <ul class="flex justify-between bg-green-400 text-black">
+      <div class="flex">
+        <li class="p-2">
+          <UButton @click="isOpen = true">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              class="size-5"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </UButton>
+          <USlideover v-model="isOpen" :overlay="false" side="left" width="5xl">
+            <div class="flex flex-col">
+              <ul>
+                <li class="h-6"></li>
+                <li>
+                  <NuxtLink to="/">
+                    <div
+                      class="flex p-2 text-xl md:hover:bg-green-400 hover:text-black hover:cursor-pointer"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        class="size-5 mt-1 mx-2"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                          clip-rule="evenodd"
+                        />
+                      </svg>
+                      <p>Home</p>
+                    </div>
+                  </NuxtLink>
+                </li>
+                <li>
                   <div
-                    class="flex p-2 text-xl md:hover:bg-green-400 hover:text-black hover:cursor-pointer"
+                    class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -37,88 +66,90 @@ const isOpen = ref(false);
                     >
                       <path
                         fill-rule="evenodd"
-                        d="M9.293 2.293a1 1 0 0 1 1.414 0l7 7A1 1 0 0 1 17 11h-1v6a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3a1 1 0 0 0-1-1H9a1 1 0 0 0-1 1v3a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1v-6H3a1 1 0 0 1-.707-1.707l7-7Z"
+                        d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 1-11-4.69v.447a3.5 3.5 0 0 0 1.025 2.475L8.293 10 8 10.293a1 1 0 0 0 0 1.414l1.06 1.06a1.5 1.5 0 0 1 .44 1.061v.363a1 1 0 0 0 .553.894l.276.139a1 1 0 0 0 1.342-.448l1.454-2.908a1.5 1.5 0 0 0-.281-1.731l-.772-.772a1 1 0 0 0-1.023-.242l-.384.128a.5.5 0 0 1-.606-.25l-.296-.592a.481.481 0 0 1 .646-.646l.262.131a1 1 0 0 0 .447.106h.188a1 1 0 0 0 .949-1.316l-.068-.204a.5.5 0 0 1 .149-.538l1.44-1.234A6.492 6.492 0 0 1 16.5 10Z"
                         clip-rule="evenodd"
                       />
                     </svg>
-                    <p>Home</p>
+                    <p>Campaigns</p>
                   </div>
-                </NuxtLink>
-              </li>
-              <li>
-                <div
-                  class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="size-5 mt-1 mx-2"
+                </li>
+                <li>
+                  <div
+                    class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
                   >
-                    <path
-                      fill-rule="evenodd"
-                      d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 1-11-4.69v.447a3.5 3.5 0 0 0 1.025 2.475L8.293 10 8 10.293a1 1 0 0 0 0 1.414l1.06 1.06a1.5 1.5 0 0 1 .44 1.061v.363a1 1 0 0 0 .553.894l.276.139a1 1 0 0 0 1.342-.448l1.454-2.908a1.5 1.5 0 0 0-.281-1.731l-.772-.772a1 1 0 0 0-1.023-.242l-.384.128a.5.5 0 0 1-.606-.25l-.296-.592a.481.481 0 0 1 .646-.646l.262.131a1 1 0 0 0 .447.106h.188a1 1 0 0 0 .949-1.316l-.068-.204a.5.5 0 0 1 .149-.538l1.44-1.234A6.492 6.492 0 0 1 16.5 10Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <p>Campaigns</p>
-                </div>
-              </li>
-              <li>
-                <div
-                  class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="size-5 mt-1 mx-2"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5Zm2.25 8.5a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Zm0 3a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="size-5 mt-1 mx-2"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.5 2A1.5 1.5 0 0 0 3 3.5v13A1.5 1.5 0 0 0 4.5 18h11a1.5 1.5 0 0 0 1.5-1.5V7.621a1.5 1.5 0 0 0-.44-1.06l-4.12-4.122A1.5 1.5 0 0 0 11.378 2H4.5Zm2.25 8.5a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Zm0 3a.75.75 0 0 0 0 1.5h6.5a.75.75 0 0 0 0-1.5h-6.5Z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
 
-                  <p>Characters</p>
-                </div>
-              </li>
-              <li>
-                <div
-                  class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="size-5 mt-1 mx-2"
+                    <p>Characters</p>
+                  </div>
+                </li>
+                <li>
+                  <div
+                    class="flex p-2 text-xl hover:bg-green-400 hover:text-black hover:cursor-pointer"
                   >
-                    <path
-                      d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z"
-                    />
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      class="size-5 mt-1 mx-2"
+                    >
+                      <path
+                        d="M10 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM3.465 14.493a1.23 1.23 0 0 0 .41 1.412A9.957 9.957 0 0 0 10 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 0 0-13.074.003Z"
+                      />
+                    </svg>
 
-                  <p>My Profile</p>
-                </div>
-              </li>
-            </ul>
-          </div>
-          <div class="p-4 flex-1">
-            <UButton
-              color="gray"
-              variant="ghost"
-              size="sm"
-              icon="i-heroicons-x-mark-20-solid"
-              class="flex sm:hidden absolute end-5 top-5 z-10"
-              square
-              padded
-              @click="isOpen = false"
+                    <p>My Profile</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="p-4 flex-1">
+              <UButton
+                color="gray"
+                variant="ghost"
+                size="sm"
+                icon="i-heroicons-x-mark-20-solid"
+                class="flex sm:hidden absolute end-5 top-5 z-10"
+                square
+                padded
+                @click="isOpen = false"
+              />
+            </div>
+          </USlideover>
+        </li>
+        <li class="py-2 text-xl">Fables</li>
+      </div>
+      <div class=":hover cursor-pointer" @click="logout">
+        <li class="py-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            class="size-5 mt-1 mx-2"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M3 4.25A2.25 2.25 0 0 1 5.25 2h5.5A2.25 2.25 0 0 1 13 4.25v2a.75.75 0 0 1-1.5 0v-2a.75.75 0 0 0-.75-.75h-5.5a.75.75 0 0 0-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 0 0 .75-.75v-2a.75.75 0 0 1 1.5 0v2A2.25 2.25 0 0 1 10.75 18h-5.5A2.25 2.25 0 0 1 3 15.75V4.25Z"
+              clip-rule="evenodd"
             />
-          </div>
-        </USlideover>
-      </li>
-      <li class="py-2 text-xl">Fables</li>
+            <path
+              fill-rule="evenodd"
+              d="M6 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H6.75A.75.75 0 0 1 6 10Z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </li>
+      </div>
     </ul>
   </nav>
 </template>
