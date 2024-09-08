2cab696434ac18e2cb04fdd5c90365bb9d6a00ff
Tom Slanda <jesus_slanda@yahoo.com>
2024-09-08 12:00:55 -0400

    updated tailwind config and favicon (#8)
    Co-authored-by: Tom Slanda <tomslanda@Toms-MacBook-Air.local>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './pages/**/*.{vue,js,ts}',
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.{vue,js,ts}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     extend: {
+        fontFamily: {
+          sans: ['EB Garamond', 'ui-serif', 'system-ui'],
+        },
+        fontSize: {
+          base: '18px',
+        },
+      },
     },
-  },
   plugins: [],
 }
 
