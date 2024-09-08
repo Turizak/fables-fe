e51f7644633d518704a4bf055b27804e0d6c7aa7
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-09-08 15:51:28 -0400

    fixed token refresh

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'login' as 'default',
+});
+</script>
 
 <template>
   <h1 class="flex justify-center text-5xl p-2">Login</h1>
