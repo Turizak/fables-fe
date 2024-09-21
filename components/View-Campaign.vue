ddd56474a21d4b7fb0c2e9397eb6378b191f5975
slandath <jesus_slanda@yahoo.com>
2024-09-20 23:03:16 -0400

    typescript

--- components/View-Campaign.vue
+++ components/View-Campaign.vue
@@ -1,9 +1,36 @@
-<script setup>
-import { useAuthStore } from "#imports";
+<script setup lang="ts">
+type Timestamp = {
+  time: string;
+  valid: boolean;
+};
+
+type Campaign = {
+  uuid: string;
+  name: string;
+  creatorUuid: string;
+  dmUuid: string;
+  partyUuids: string[];
+  completed: boolean;
+  active: boolean;
+  ruleset: string;
+  maxPlayers: number;
+  created: Timestamp;
+  lastUpdated: Timestamp | null;
+};
+
+type ApiResponse = {
+  data: {
+    campaign: Campaign;
+  };
+  message: string;
+  status: number;
+  statusText: string;
+  timestamp: string;
+};
 const config = useRuntimeConfig();
 const authStore = useAuthStore();
 
-const { data } = await useFetch(
+const { data } = await useFetch<ApiResponse>(
   config.public.baseURL + "/campaign/" + "8335f613-ff58-496c-868d-3ab850ee949f",
   {
     method: "GET",
@@ -13,20 +40,21 @@ const { data } = await useFetch(
     },
   },
 );
+const campaign = data.value?.data.campaign ?? ({} as Campaign);
 </script>
 
 <template>
   <UCard>
     <template #header>
-      <h3 class="text-xl">{{ data.data.campaign.name }}</h3>
+      <h3 class="text-xl">{{ campaign.name }}</h3>
     </template>
     <div class="flex gap-2">
       <UIcon name="heroicons:user-group-solid" class="w-5 h-5" />
-      <p>Party Size: {{ data.data.campaign.maxPlayers }}</p>
+      <p>Party Size: {{ campaign.maxPlayers }}</p>
     </div>
     <div class="flex gap-2">
       <UIcon name="material-symbols-light:book-2-rounded" class="w-5 h-5" />
-      <p>Ruleset: {{ data.data.campaign.ruleset }}</p>
+      <p>Ruleset: {{ campaign.ruleset }}</p>
     </div>
 
     <template #footer>
