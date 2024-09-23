75b8bd26720d10a30ec852dfea9873b8b9cb0214
slandath <jesus_slanda@yahoo.com>
2024-09-22 22:00:52 -0400

    sidemenu fixed

--- components/SideMenu.vue
+++ components/SideMenu.vue
@@ -14,7 +14,7 @@ const slideover = useSlideover();
       <ul>
         <li class="h-6" />
         <li>
-          <NuxtLink to="/">
+          <NuxtLink to="/" @click="slideover.close">
             <div
               class="flex p-2 ml-2 gap-2 md:hover:bg-green-400 hover:text-black hover:cursor-pointer"
             >
@@ -24,13 +24,13 @@ const slideover = useSlideover();
           </NuxtLink>
         </li>
         <li>
-          <div class="flex p-2 ml-2 gap-2">
+          <div class="flex p-2 ml-2 gap-2 bg-gray-700">
             <UIcon name="material-symbols:globe" class="w-5 h-5 mt-1" />
             <p class="text-xl">Campaigns</p>
           </div>
         </li>
         <li>
-          <NuxtLink to="/create-campaign">
+          <NuxtLink to="/create-campaign" @click="slideover.close">
             <div
               class="flex p-2 ml-2 gap-2 hover:bg-green-400 hover:text-black hover:cursor-pointer"
             >
@@ -43,7 +43,7 @@ const slideover = useSlideover();
           </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/view-campaigns">
+          <NuxtLink to="/view-campaigns" @click="slideover.close">
             <div
               class="flex p-2 ml-2 gap-2 hover:bg-green-400 hover:text-black hover:cursor-pointer"
             >
@@ -56,12 +56,21 @@ const slideover = useSlideover();
           </NuxtLink>
         </li>
         <li>
-          <NuxtLink to="/cchar/cchar-1">
+          <div class="flex p-2 ml-2 gap-2 bg-gray-700">
+            <UIcon name="heroicons:user-group-solid" class="w-5 h-5 mt-1" />
+            <p class="text-xl">Characters</p>
+          </div>
+        </li>
+        <li>
+          <NuxtLink to="/cchar/cchar-1" @click="slideover.close">
             <div
               class="flex p-2 ml-2 gap-2 hover:bg-green-400 hover:text-black hover:cursor-pointer"
             >
-              <UIcon name="material-symbols:person-book" class="w-5 h-5 mt-1" />
-              <p class="text-xl">Characters</p>
+              <UIcon
+                name="material-symbols:person-book"
+                class="w-5 h-5 mt-1 ml-4"
+              />
+              <p class="text-lg">Create a Character</p>
             </div>
           </NuxtLink>
         </li>
