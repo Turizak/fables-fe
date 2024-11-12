421d10c82e2512db41bcc1cdce1f6ba203032b6f
slandath <jesus_slanda@yahoo.com>
2024-11-12 18:36:15 -0500

    POST successful

--- components/Location-Form.vue
+++ components/Location-Form.vue
@@ -4,7 +4,7 @@ import { locationValidate } from "~/utils/location-validation";
 import type { FormSubmitEvent } from "#ui/types";
 import type { AuthResponse, LocationForm } from "~/types/types";
 
-// const config = useRuntimeConfig();
+const config = useRuntimeConfig();
 const authStore = useAuthStore();
 const toast = useToast();
 
@@ -12,26 +12,35 @@ const route = useRoute();
 const uuid = route.params.uuid;
 
 const state = reactive({
-  campaign: `${uuid}`,
   location: undefined,
   description: undefined,
 });
 
+const buttonText = reactive({
+  submitButton: "Add Location",
+  backButton: "View Campaign",
+});
+
 const loading = ref(false);
 const disabled = ref(false);
-const buttonText = ref("Add Location");
+
+async function goBack() {
+  await authStore.ensureValidToken();
+  await navigateTo(`/campaign/${uuid}/view-campaign`);
+}
 
 async function onSubmit(event: FormSubmitEvent<LocationForm>) {
   loading.value = true;
   disabled.value = true;
-  buttonText.value = "Adding...";
+  buttonText.submitButton = "Adding...";
+  buttonText.backButton = "";
   try {
     const response: AuthResponse = await $fetch(
-      // config.public.baseURL + "/api/campaign/" + uuid + "/location/create",
-      "https://httpbin.org/post",
+      config.public.baseURL + "/campaign/" + uuid + "/location/create",
       {
         method: "POST",
         headers: {
+          Authorization: `Bearer ${authStore.token}`,
           "Content-Type": "application/json",
         },
         body: {
@@ -40,33 +49,26 @@ async function onSubmit(event: FormSubmitEvent<LocationForm>) {
         },
       },
     );
-    authStore.setToken(response.data.tokens.accessToken);
-    authStore.setRefreshToken(response.data.tokens.refreshToken);
-    await navigateTo(`/campaign/${uuid}/view-campaign`);
-  } catch (error: unknown) {
-    if (error && typeof error === "object" && "response" in error) {
-      const errResponse = (
-        error as {
-          response: {
-            _data: { message: string; status: number; statusText: string };
-          };
-        }
-      ).response;
-      toast.add({
-        title: `${errResponse._data.message}`,
-        color: "red",
-        icon: "i-heroicons-x-circle-solid",
-      });
-      console.error(
-        `Error: ${errResponse._data.status}, ${errResponse._data.statusText}`,
-      );
-    } else {
-      console.error(error);
-    }
+    console.log(response);
+    buttonText.submitButton = "Success!";
+    toast.add({
+      title: "Location Added!",
+      icon: "i-heroicons-check-circle-solid",
+    });
+  } catch (err) {
+    console.error(err);
+    toast.add({
+      title: "There was an error - please try again",
+      color: "red",
+      icon: "i-heroicons-x-circle-solid",
+    });
   } finally {
     loading.value = false;
     disabled.value = false;
-    buttonText.value = "Add Location";
+    buttonText.submitButton = "Add Location";
+    buttonText.backButton = "View Campaign";
+    state.location = undefined;
+    state.description = undefined;
   }
 }
 </script>
@@ -85,13 +87,23 @@ async function onSubmit(event: FormSubmitEvent<LocationForm>) {
       <UFormGroup label="Description" name="description" class="mb-4">
         <UTextarea v-model="state.description" :disabled="disabled" />
       </UFormGroup>
+      <UButton
+        block
+        color="amber"
+        variant="solid"
+        class="p-2 box-border text-white inline-flex h-[35px] items-center justify-center rounded-[4px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[20px]"
+        :loading="loading"
+        @click="goBack"
+      >
+        {{ buttonText.backButton }}</UButton
+      >
 
       <UButton
         type="submit"
         class="p-2 box-border w-full text-white inline-flex h-[35px] items-center justify-center rounded-[4px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none mt-[20px]"
         :loading="loading"
       >
-        {{ buttonText }}</UButton
+        {{ buttonText.submitButton }}</UButton
       >
     </UForm>
   </div>
--- utils/location-validation.ts
+++ utils/location-validation.ts
@@ -4,5 +4,6 @@ import type { LocationForm } from "~/types/types";
 export const locationValidate = (state: LocationForm): FormError[] => {
     const errors = [];
     if (!state.location) errors.push({ path: "location", message: "Required" });
+    if (!state.description) errors.push({ path: "description", message: "Required" });
     return errors;
   };
