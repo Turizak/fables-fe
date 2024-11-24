fd6057ff1686bd908a77f0b06cdc6303b0b8f538
Tom Slanda <jesus_slanda@yahoo.com>
2024-11-24 10:54:37 -0500

    feature/toastIcons (#46)
    * create campaign form done
    
    * login form done
    
    * npc form done
    
    * session form done

--- components/NPC-Form.vue
+++ components/NPC-Form.vue
@@ -107,14 +107,14 @@ async function onSubmit(event: FormSubmitEvent<NPCForm>) {
     state.submitButton = "Success!";
     toast.add({
       title: "NPC Added!",
-      icon: "i-heroicons-check-circle-solid",
+      icon: "i-material-symbols-light:check-circle",
     });
   } catch (err) {
     console.error(err);
     toast.add({
       title: "There was an error - please try again",
       color: "red",
-      icon: "i-heroicons-x-circle-solid",
+      icon: "i-material-symbols-light:cancel",
     });
   } finally {
     state.loading = false;
--- components/Session-Form.vue
+++ components/Session-Form.vue
@@ -79,14 +79,14 @@ async function onSubmit(event: FormSubmitEvent<SessionForm>) {
     state.submitButton = "Success!";
     toast.add({
       title: "Session Added!",
-      icon: "i-heroicons-check-circle-solid",
+      icon: "i-material-symbols-light:check-circle",
     });
   } catch (err) {
     console.error(err);
     toast.add({
       title: "There was an error - please try again",
       color: "red",
-      icon: "i-heroicons-x-circle-solid",
+      icon: "i-material-symbols-light:cancel",
     });
   } finally {
     state.loading = false;
