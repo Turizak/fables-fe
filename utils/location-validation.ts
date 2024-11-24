9ea9987ba6eb054002923ccd44dae1c8af879144
slandath <jesus_slanda@yahoo.com>
2024-11-24 11:53:44 -0500

    location validation done

--- utils/location-validation.ts
+++ utils/location-validation.ts
@@ -4,6 +4,8 @@ import type { LocationForm } from "~/types/types";
 export const locationValidate = (state: LocationForm): FormError[] => {
     const errors = [];
     if (!state.location) errors.push({ path: "location", message: "Required" });
+    if (state.location.length > 99) errors.push({path: "location", message: "Maximum 100 characters."})
     if (!state.description) errors.push({ path: "description", message: "Required" });
+    if (state.description.length > 99) errors.push({path: "description", message: "Maximum 100 characters."})
     return errors;
   };
