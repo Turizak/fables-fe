0a64e2ac9f176eb067983fbc87433968b2e1fc20
slandath <jesus_slanda@yahoo.com>
2024-12-09 20:21:29 -0500

    select NPC page created and links updated

---
+++ pages/campaign/[campaignUuid]/session/[sessionUuid]/select-npc.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SelectNPCForm from "~/components/forms/select/Select-NPC-Form.vue";
+definePageMeta({
+  middleware: "fresh-token",
+});
+</script>
+
+<template>
+  <div>
+    <h1 class="flex justify-center text-5xl p-2 mb-2">Select NPC</h1>
+    <SelectNPCForm />
+  </div>
+</template>
