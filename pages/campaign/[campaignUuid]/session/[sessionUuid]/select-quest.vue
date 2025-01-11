fe97011bd6723914fbc143999620f66e3c283736
slandath <jesus_slanda@yahoo.com>
2025-01-10 23:08:56 -0500

    created quest select page

---
+++ pages/campaign/[campaignUuid]/session/[sessionUuid]/select-quest.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SelectQuestForm from "~/components/forms/select/Select-Quest-Form.vue";
+definePageMeta({
+  middleware: "fresh-token",
+});
+</script>
+
+<template>
+  <div>
+    <h1 class="flex justify-center text-5xl p-2 mb-2">Select Quest</h1>
+    <SelectQuestForm />
+  </div>
+</template>
